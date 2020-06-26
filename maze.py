f71073a06c50bea3ab12b054e66fec489e84e5f0
Shubhang Agrawal <shubhangagrawal1999@gmail.com>
2020-06-27 01:59:03 +0530

    Maze.py

---
+++ maze.py
@@ -0,0 +1,123 @@
+import queue
+
+# Maze Structure
+
+def Maze():
+    maze = []
+    maze.append(["#","#", "#", "#", "#", "S", "#", "#", "#"])
+    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
+    maze.append(["#"," ", "#", "#", " ", "#", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", " ", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", "#", "#"])
+    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
+    maze.append(["#","#", "E", "#", "#", "#", "#", "#", "#"])
+
+    return maze
+
+
+def displayMaze(maze, path=""):
+    for x, pos in enumerate(maze[0]):
+        if pos == "S":
+            start = x
+
+    i = start
+    j = 0
+    pos = set()
+    for move in path:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+        pos.add((j, i))
+    
+    for j, row in enumerate(maze):
+        for i, col in enumerate(row):
+            if (j, i) in pos:
+                print("- ", end="")
+            else:
+                print(col + " ", end="")
+        print()
+        
+
+
+def valid(maze, moves):
+    for x, pos in enumerate(maze[0]):
+        if pos == "S":
+            start = x
+
+    i = start
+    j = 0
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+        if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):
+            return False
+        elif (maze[j][i] == "#"):
+            return False
+
+    return True
+
+
+def End(maze, moves):
+    for x, pos in enumerate(maze[0]):
+        if pos == "S":
+            start = x
+
+    i = start
+    j = 0
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+    if maze[j][i] == "E":
+        print("\n------------------\nFound: " + moves + "\n")
+        print("Solution")
+        displayMaze(maze, moves)
+        return True
+
+    return False
+
+
+# MAIN ALGORITHM
+
+nums = queue.Queue()
+nums.put("")
+add = ""
+maze  = Maze()
+print("\nProblem")
+displayMaze(maze, path="")
+
+while not End(maze, add): 
+    add = nums.get()
+    for j in ["L", "R", "U", "D"]:
+        put = add + j
+        if valid(maze, put):
+            nums.put(put)
